b727ee063ea1463b932cb5b418bbddf774bea742
wushuping <ww>
2021-06-28 15:36:12 +0800

    删除target

--- src/main/java/com/w/t/controller/IndexController.java
+++ src/main/java/com/w/t/controller/IndexController.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * @Packagename com.w.t
  * @Classname IndexController
@@ -15,9 +19,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class IndexController {
 
-    @ResponseBody
+//    @ResponseBody
     @RequestMapping("/index")
-    public String toIndex() {
-        return "Welcome !";
+    public String toIndex(HttpServletRequest request, HttpServletResponse response) {
+        request.setAttribute("Authorization","eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI1MjE2IiwiaWF0IjoxNjIxMjIwMTkwLCJzdWIiOiJ7XCJyVHlwZVwiOlwiMFwiLFwidVR5cGVcIjpcIjBcIixcInBSSWRcIjpcIjY5NlwiLFwib3JnTGV2ZWxcIjpcIjFcIixcImlkXCI6XCI1MjE2XCIsXCJySWRcIjpcIjY5NlwiLFwib3JnSWRcIjpcIjc4MlwiLFwidVNvdXJjZVwiOlwiMFwifSIsImV4cCI6MTYyMTIyMzc5MH0.iQO82VFIwCT9IYJESGWxK1oYkO0q0X8xZkv3UQkgH0c");
+        return "redirect:http://jimu.smartsteps.com/#/app/insight/crowdHeat";
     }
 }
